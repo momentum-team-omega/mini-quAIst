0b246b5c5a0d65734ae559be54b347856b948cfd
Nathan Vogt <merrick.vogt@gmail.com>
2023-10-08 14:15:07 -0400

    created Dialogue component and styled it with custom css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import './App.css';
-import Home from 'components/Home';
-import Login from 'components/Login';
-import Register from 'components/Register';
-import Game from 'components/Game';
-import BattleTemp from 'components/Battle-temp';
+import React, { useState } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import "./App.css";
+import Home from "components/Home";
+import Login from "components/Login";
+import Register from "components/Register";
+import Game from "components/Game";
+import BattleTemp from "components/Battle-temp";
+import Dialogue from "./components/Dialogue";
 
 function App() {
   const [token, setToken] = useState(false);
@@ -14,22 +15,38 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Navigate to='/home' replace />} />;
-        <Route path='/home' element={<Home />} />;
+        <Route path="/" element={<Navigate to="/home" replace />} />;
+        <Route path="/home" element={<Home />} />;
         <Route
-          path='/login'
+          path="/login"
           element={<Login setToken={setToken} setIsLoggedIn={setIsLoggedIn} />}
         />
         ;
         <Route
-          path='/register'
+          path="/register"
           element={
             <Register setToken={setToken} setIsLoggedIn={setIsLoggedIn} />
           }
         />
         ;
-        <Route path='/play' element={<Game />} />;
-        <Route path='/battle' element={<BattleTemp />} />;
+        <Route path="/play" element={<Game />} />;
+        <Route path="/battle" element={<BattleTemp />} />;
+        <Route
+          path="/dialogue"
+          element={
+            <Dialogue
+              npcText="Hello, adventurer! How can I help you?"
+              options={[
+                "Ask about quests",
+                "Buy items",
+                "Leave",
+                "Say goodbye",
+              ]}
+              backgroundImage="/src/assets/lotr.png" // Pass the URL as a prop
+            />
+          }
+        />
+        ;
       </Routes>
     </>
   );
