3db3a33f14d20d1a3b1f65374a7e5a46c5aee7a3
duffing09 <duffing18@gmail.com>
2023-10-09 11:08:35 -0400

    started create character component with images

--- src/components/Create_Char.jsx
+++ src/components/Create_Char.jsx
@@ -1,9 +1,45 @@
-const Create_Char = () => {
+import React, { useState } from 'react';
+import barbCharacter from '/src/assets/barbarian-character.png';
+import mageCharacter from '/src/assets/mage-character.png';
+import rogueCharacter from '/src/assets/rogue-character.png';
+const SelectCharacter = ({ onCharacterSelect }) => {
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
+  const handleCharacterSelect = (character) => {
+    setSelectedCharacter(character);
+  };
   return (
-    <div>
-      <h1>Character Creation</h1>
+    <div className="select-character">
+      <h2>Select Your Character</h2>
+      <div className="character-options">
+        <div
+          className={`character-option ${
+            selectedCharacter === 'mage' ? 'selected' : ''
+          }`}
+          onClick={() => handleCharacterSelect('mage')}
+        >
+          <img className="char-container" src={mageCharacter} alt="Mage Character" />
+          <p>Mage</p>
+        </div>
+        <div
+          className={`character-option ${
+            selectedCharacter === 'barbarian' ? 'selected' : ''
+          }`}
+          onClick={() => handleCharacterSelect('barbarian')}
+        >
+          <img className="char-container" src={barbCharacter} alt="Barbarian Character" />
+          <p>Barbarian</p>
+        </div>
+        <div
+          className={`character-option ${
+            selectedCharacter === 'rogue' ? 'selected' : ''
+          }`}
+          onClick={() => handleCharacterSelect('rogue')}
+        >
+          <img className="char-container" src={rogueCharacter} alt="Rogue Character" />
+          <p>Rogue</p>
+        </div>
+      </div>
     </div>
   );
 };
-
-export default Create_Char;
+export default SelectCharacter;
