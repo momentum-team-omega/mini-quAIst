02470ca948dc6794437727bfc03fec74b2c83f1e
davis <davisfpatterson@gmail.com>
2023-10-09 02:03:06 -0400

    refactored map again and overlayed collision points. collision detection still not working so collision points commented out currently. also reworked movement, holding down movement keys now triggers smooth movement.

--- src/components/Char_Move.jsx
+++ src/components/Char_Move.jsx
@@ -1,67 +1,108 @@
 import { useEffect, useContext } from 'react';
-import MapContext from 'contexts/MapContext';
+import CollisionContext from 'contexts/CollisionContext';
 
-const Char_Move = ({ position, setPosition, MAP_SIZE }) => {
-  const { mapData, setMapData } = useContext(MapContext);
-  const unwalkableTiles = ['darkgreen', 'blue', 'brown', 'papayawhip'];
+const Char_Move = ({ position, setPosition, TILE_SIZE, MOVE_SPEED }) => {
+  const collisionMap = useContext(CollisionContext);
 
-  // const isWalkable = (x, y) => {
-  //   if (mapData) {
-  //     if (x < 0 || x >= MAP_SIZE || y < 0 || y >= MAP_SIZE) {
-  //       return false;
-  //     }
+  const keys = {
+    w: { pressed: false },
+    a: { pressed: false },
+    s: { pressed: false },
+    d: { pressed: false },
+  };
 
-  //     const roundedX = Math.round(x);
-  //     const roundedY = Math.round(y);
+  // const checkCollision = (newX, newY) => {
 
-  //     return !unwalkableTiles.includes(mapData[roundedY][roundedX]);
-  //   }
+  //   const gridX = Math.floor(newX / TILE_SIZE);
+  //   const gridY = Math.floor(newY / TILE_SIZE);
+
+  //   console.log(`Checking collision at gridX: ${gridX}, gridY: ${gridY}`);
+
+  //   return collisionMap[gridY][gridX] === 1025;
   // };
 
-  useEffect(() => {
-    const handleKeyPress = (event) => {
-      let newX = position.x;
-      let newY = position.y;
+  const moveCharacter = () => {
+    setPosition((currentPos) => {
+      let newX = currentPos.x;
+      let newY = currentPos.y;
 
-      switch (event.key) {
+      if (keys.w.pressed) newY -= MOVE_SPEED;
+      if (keys.a.pressed) newX -= MOVE_SPEED;
+      if (keys.s.pressed) newY += MOVE_SPEED;
+      if (keys.d.pressed) newX += MOVE_SPEED;
+
+      // if (checkCollision(newX, currentPos.y)) newX = currentPos.x;
+      // if (checkCollision(currentPos.x, newY)) newY = currentPos.y;
+
+      // console.log(
+      //   `Moving from x: ${currentPos.x}, y: ${currentPos.y} to x: ${newX}, y: ${newY}`
+      // );
+
+      return { x: newX, y: newY };
+    });
+  };
+
+  useEffect(() => {
+    const handleDownKey = (e) => {
+      e.preventDefault();
+
+      switch (e.key) {
         case 'ArrowUp':
         case 'w':
-          newY -= 1;
-          event.preventDefault();
-          break;
-        case 'ArrowDown':
-        case 's':
-          newY += 1;
-          event.preventDefault();
+          keys.w.pressed = true;
           break;
         case 'ArrowLeft':
         case 'a':
-          newX -= 1;
-          event.preventDefault();
+          keys.a.pressed = true;
+          break;
+        case 'ArrowDown':
+        case 's':
+          keys.s.pressed = true;
           break;
         case 'ArrowRight':
         case 'd':
-          newX += 1;
-          event.preventDefault();
-          break;
-        default:
+          keys.d.pressed = true;
           break;
       }
-
-      // if (isWalkable(newX, newY)) {
-      setPosition({ x: newX, y: newY });
-      console.log('position: ', position);
-      // } else {
-      // console.log('Collision detected at:', newX, newY);
-      // }
     };
 
-    window.addEventListener('keydown', handleKeyPress);
+    const handleUpKey = (e) => {
+      switch (e.key) {
+        case 'ArrowUp':
+        case 'w':
+          keys.w.pressed = false;
+          break;
+        case 'ArrowLeft':
+        case 'a':
+          keys.a.pressed = false;
+          break;
+        case 'ArrowDown':
+        case 's':
+          keys.s.pressed = false;
+          break;
+        case 'ArrowRight':
+        case 'd':
+          keys.d.pressed = false;
+          break;
+      }
+    };
+
+    window.addEventListener('keydown', handleDownKey);
+    window.addEventListener('keyup', handleUpKey);
 
     return () => {
-      window.removeEventListener('keydown', handleKeyPress);
+      window.removeEventListener('keydown', handleDownKey);
+      window.removeEventListener('keyup', handleUpKey);
     };
-  }, [position]);
+  }, []);
+
+  useEffect(() => {
+    const moveInterval = setInterval(moveCharacter, 10);
+
+    return () => {
+      clearInterval(moveInterval);
+    };
+  }, []);
 
   return null;
 };
