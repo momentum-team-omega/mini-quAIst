976aa56d8420f8d2c4a0925e2e7dd8c568d4828b
Jaylan Wilkins <jay.wilkins23@yahoo.com>
2023-10-09 22:38:15 -0400

    functional dice, without styling

--- src/components/Ability_Check.jsx
+++ src/components/Ability_Check.jsx
@@ -1,10 +1,21 @@
-const Ability_Check = () => {
+import React, { useState } from "react";
+
+const TwentySidedDie = () => {
+  const [diceRoll, setDiceRoll] = useState(null);
+
+  const rollDie = () => {
+    const newRoll = Math.floor(Math.random() * 20) +1;
+    setDiceRoll(newRoll);
+  };
+
   return (
-    <div>
+    <div className="dice-container">
       <h1>Ability Check</h1>
-      <h3>(dice roll)</h3>
+      <div className="twenty-sided-die" onClick={rollDie}>{diceRoll !== null && <div className="rolled-number">{diceRoll}</div>}
+      </div>
     </div>
   );
 };
 
-export default Ability_Check;
+export default TwentySidedDie;
+
