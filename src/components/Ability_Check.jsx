6bebc26ff80b2f280530c8a90254d919bf079131
Jaylan Wilkins <jay.wilkins23@yahoo.com>
2023-10-11 16:34:00 -0400

    notes

--- src/components/Ability_Check.jsx
+++ src/components/Ability_Check.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 
 const TwentySidedDie = () => {
+  // variable, declaration and assigment
   const [diceRoll, setDiceRoll] = useState(null);
-
-  const rollDie = () => {
+// useState pulls an array with 2 things from it. State variable & updater function
+// null value being passed thru is a defualt value for dice roll
+  const rollDie = (event) => {
+    console.log(event)
     const newRoll = Math.floor(Math.random() * 20) +1;
     setDiceRoll(newRoll);
+    // calling the state setter function and giving it a new value
   };
 
   return (
