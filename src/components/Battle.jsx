aa2cf55b02ee911c11000d98e256cbb6035e3b64
Chris Riddle <stopimmediatley@gmail.com>
2023-10-11 20:06:25 -0400

    deleted duplicate notes

--- src/components/Battle.jsx
+++ src/components/Battle.jsx
@@ -108,7 +108,9 @@ const Battle = () => {
         setIsPaused(false);
 
         // Trigger the health change and flicker
-        handleHealthChange(newPlayerHealth, "smack");
+        if (someoneDied === false) {
+          handleHealthChange(newPlayerHealth, "smack");
+        }
       }, 1700);
 
       handleEnemyHealthChange(opponentHealth - playerDamage, "smack");
