1d4e1ea9f1b7ba8f10e0b59f9a205832d4eb82b0
Chris Riddle <stopimmediately@gmail.com>
2023-10-12 18:22:35 -0400

    added picture slideshow to Game_Start

--- src/components/Game_Start.jsx
+++ src/components/Game_Start.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Game_Start = () => {
-  return <h1>Game Start</h1>;
+  const [imageIndex, setImageIndex] = useState(0);
+  const imageUrls = [
+    "/src/assets/gamestart-assets/LoraEx1.jpeg",
+    "/src/assets/gamestart-assets/LoraEx2.jpeg",
+    "/src/assets/gamestart-assets/LoraEx3.jpeg",
+  ];
+
+  useEffect(() => {
+    const changeImage = () => {
+      setImageIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
+    };
+
+    const intervalId = setInterval(changeImage, 10000);
+
+    // Clean up the interval when the component unmounts
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <>
+      <h1>Game Start</h1>
+      <div className='image-container'>
+        <img src={imageUrls[imageIndex]} alt={`Image ${imageIndex + 1}`} />
+      </div>
+    </>
+  );
 };
 
 export default Game_Start;
