964af5dd84b0743c022799b66966795616beaf82
Chris Riddle <stopimmediately@gmail.com>
2023-10-18 16:17:02 -0400

    dialogue style update, changed positioning and text size, set image in new smaller container and replaced placeholder assetts

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,17 +1,17 @@
-import React, { useContext, useState } from 'react';
-import Nav from 'components/Nav';
-import Overworld from './Overworld';
-import Dialogue from './Dialogue';
-import Create_Char from './Create_Char';
-import GameContext from './GameContext';
-import Battle from './Battle';
+import React, { useContext, useState } from "react";
+import Nav from "components/Nav";
+import Overworld from "./Overworld";
+import Dialogue from "./Dialogue";
+import Create_Char from "./Create_Char";
+import GameContext from "./GameContext";
+import Battle from "./Battle";
 
 const Game = () => {
-  const [scene, setScene] = useState('overworld');
-  const [currentNPC, setCurrentNPC] = useState('troll');
+  const [scene, setScene] = useState("overworld");
+  const [currentNPC, setCurrentNPC] = useState("troll");
   const [charStats, setCharStats] = useState({
-    name: 'game test',
-    charClass: 'mage',
+    name: "game test",
+    charClass: "mage",
     health: 33,
     strength: 6,
     str_mod: -2,
@@ -21,15 +21,14 @@ const Game = () => {
     dex_mod: 0,
   });
 
-  const [typeOfCheck, setTypeOfCheck] = useState('wis');
-  const [outcome, setOutcome] = useState('');
+  const [typeOfCheck, setTypeOfCheck] = useState("wis");
+  const [outcome, setOutcome] = useState("");
   const [makeCheck, setMakeCheck] = useState(false);
-  const [currentMap, setCurrentMap] = useState('start');
-
+  const [currentMap, setCurrentMap] = useState("testMap");
 
   const [gameWindow, setGameWindow] = useState({
-    height: '720px',
-    width: '1280px',
+    height: "720px",
+    width: "1280px",
   });
 
   return (
@@ -59,17 +58,17 @@ const Game = () => {
               width: gameWindow.width,
             }}
           >
-            {scene === 'overworld' && (
+            {scene === "overworld" && (
               <Overworld
                 currentMap={currentMap}
                 setCurrentMap={setCurrentMap}
               />
             )}
-            {scene === 'characterCreation' && (
+            {scene === "characterCreation" && (
               <Create_Char charStats={charStats} setCharStats={setCharStats} />
             )}
-            {scene === 'dialogue' && <Dialogue />}
-            {scene === 'battle' && <Battle />}
+            {scene === "dialogue" && <Dialogue />}
+            {scene === "battle" && <Battle />}
           </div>
         </div>
       </GameContext.Provider>
