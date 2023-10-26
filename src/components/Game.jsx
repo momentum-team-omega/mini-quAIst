658174e52fe45c8776e197ecba0e12f05632c364
Chris Riddle <stopimmediately@gmail.com>
2023-10-26 10:32:51 -0400

    reset checkpoints and change comment styling

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import SFX from 'components/SFX';
-import Vol_Icon from 'components/menu/Vol_Icon';
-import Inventory_Icon from 'components/menu/Inventory_Icon';
-import Menu_Icon from 'components/menu/Menu_Icon';
-import Overworld from 'components/Overworld';
-import Dialogue from 'components/Dialogue';
-import Create_Char from 'components/Create_Char';
-import GameContext from 'contexts/GameContext';
-import Battle from 'components/Battle';
-import Cut_Scene from 'components/Cut_Scene';
-import Menu_Keys from 'components/Menu_Keys';
+import React, { useState } from "react";
+import SFX from "components/SFX";
+import Vol_Icon from "components/menu/Vol_Icon";
+import Inventory_Icon from "components/menu/Inventory_Icon";
+import Menu_Icon from "components/menu/Menu_Icon";
+import Overworld from "components/Overworld";
+import Dialogue from "components/Dialogue";
+import Create_Char from "components/Create_Char";
+import GameContext from "contexts/GameContext";
+import Battle from "components/Battle";
+import Cut_Scene from "components/Cut_Scene";
+import Menu_Keys from "components/Menu_Keys";
 
 const Game = ({ mute, setMute }) => {
-  const [scene, setScene] = useState('intro');
-  const [currentMap, setCurrentMap] = useState('startHouse');
-  const [currentNPC, setCurrentNPC] = useState('');
+  const [scene, setScene] = useState("intro");
+  const [currentMap, setCurrentMap] = useState("startHouse");
+  const [currentNPC, setCurrentNPC] = useState("");
 
   const [charStats, setCharStats] = useState({
-    name: '',
-    charClass: '',
+    name: "",
+    charClass: "",
     health: null,
     strength: null,
     str_mod: null,
@@ -28,13 +28,13 @@ const Game = ({ mute, setMute }) => {
     dex_mod: null,
   });
 
-  const [typeOfCheck, setTypeOfCheck] = useState('wis');
-  const [outcome, setOutcome] = useState('');
+  const [typeOfCheck, setTypeOfCheck] = useState("wis");
+  const [outcome, setOutcome] = useState("");
   const [makeCheck, setMakeCheck] = useState(false);
 
   const [gameWindow, setGameWindow] = useState({
-    height: '720px',
-    width: '1280px',
+    height: "720px",
+    width: "1280px",
   });
 
   const [isMoving, setIsMoving] = useState(false);
@@ -42,9 +42,9 @@ const Game = ({ mute, setMute }) => {
   const [isFPressed, setIsFPressed] = useState(false);
 
   const [checkpoints, setCheckpoints] = useState({
-    1: true, // talk to wise man
-    2: true, // talk to steve
-    3: true, // talk to villageLeader
+    1: false, // talk to wise man
+    2: false, // talk to steve
+    3: false, // talk to villageLeader
     4: false, // get passed troll
     5: false, // defeat troll
   });
@@ -66,48 +66,48 @@ const Game = ({ mute, setMute }) => {
   const [npcs, setNpcs] = useState([
     {
       id: 1,
-      name: 'wiseman',
+      name: "wiseman",
       steps: 4,
       animationSpeed: 800,
       alive: true,
       triggered: false,
-      message: 'Greetings',
+      message: "Greetings",
     },
     {
       id: 2,
-      name: 'steve',
+      name: "steve",
       steps: 4,
       animationSpeed: 400,
       alive: true,
       triggered: false,
-      message: 'Crikey!',
+      message: "Crikey!",
     },
     {
       id: 3,
-      name: 'villageLeader',
+      name: "villageLeader",
       steps: 4,
       animationSpeed: 200,
       alive: true,
       triggered: false,
-      message: 'Hello There!',
+      message: "Hello There!",
     },
     {
       id: 4,
-      name: 'blacksmith',
+      name: "blacksmith",
       steps: 4,
       animationSpeed: 200,
       alive: true,
       triggered: false,
-      message: 'Greetings',
+      message: "Greetings",
     },
     {
       id: 5,
-      name: 'troll',
+      name: "troll",
       steps: 4,
       animationSpeed: 200,
       alive: true,
       triggered: false,
-      message: 'RAWR',
+      message: "RAWR",
     },
   ]);
 
@@ -170,23 +170,23 @@ const Game = ({ mute, setMute }) => {
             {!menu && !inventory && <Vol_Icon />}
             {!menu &&
               !inventory &&
-              scene !== 'intro' &&
-              scene !== 'end-chapter1' &&
-              scene !== 'death' && <Inventory_Icon />}
+              scene !== "intro" &&
+              scene !== "end-chapter1" &&
+              scene !== "death" && <Inventory_Icon />}
             {!menu &&
               !inventory &&
-              scene !== 'intro' &&
-              scene !== 'end-chapter1' &&
-              scene !== 'death' && <Menu_Icon position={'normal'} />}
-            {scene === 'intro' && <Cut_Scene sceneSelection={0} />}
-            {scene === 'end-chapter1' && <Cut_Scene sceneSelection={1} />}
-            {scene === 'death' && <Cut_Scene sceneSelection={2} />}
-            {scene === 'overworld' && <Overworld />}
-            {scene === 'characterCreation' && (
+              scene !== "intro" &&
+              scene !== "end-chapter1" &&
+              scene !== "death" && <Menu_Icon position={"normal"} />}
+            {scene === "intro" && <Cut_Scene sceneSelection={0} />}
+            {scene === "end-chapter1" && <Cut_Scene sceneSelection={1} />}
+            {scene === "death" && <Cut_Scene sceneSelection={2} />}
+            {scene === "overworld" && <Overworld />}
+            {scene === "characterCreation" && (
               <Create_Char charStats={charStats} setCharStats={setCharStats} />
             )}
-            {scene === 'dialogue' && <Dialogue />}
-            {scene === 'battle' && <Battle />}
+            {scene === "dialogue" && <Dialogue />}
+            {scene === "battle" && <Battle />}
           </div>
         </div>
       </GameContext.Provider>
